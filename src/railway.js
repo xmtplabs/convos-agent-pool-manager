daa315c794202a62ab21f5dc8410c6fdf2cb9459
Saul Carlin <saul@xmtp.com>
2026-02-08 01:51:46 -0800

    fix: put branch inside source object for Railway serviceCreate

--- src/railway.js
+++ src/railway.js
@@ -33,8 +33,7 @@ export async function createService(name) {
       input: {
         projectId,
         name,
-        source: { repo },
-        branch,
+        source: { repo, branch },
       },
     }
   );
